46b5e2a38eb22ff43fb9fad4572e35147fbd4bd9
Christian Felix <xiankaylle019@gmail.com>
2018-04-01 22:03:09 +0800

    Add project files.

---
+++ BasicOOP/ClassSample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOOP
+{
+    public class ClassSample
+    {
+        //private variable
+        private string _stringProperty;
+        //getter setter
+        public string StringProperty {
+
+            get { return _stringProperty; }
+
+            set { _stringProperty = value; }
+        }
+        //shorthand getter setter
+        public int IntProperty { get; set; }
+        
+        //void method
+        public void WriteToConsole()
+        {
+            Console.WriteLine($"String Property value: { this.StringProperty} ");
+        }
+        //override method
+        public override string ToString()
+        {
+            return $"Welcome { this.StringProperty }";
+        }
+
+    }
+}
---
+++ BasicOOP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOOP
+{
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+            //instance of Person class
+            Person person = new Person();
+        }
+    }
+}
